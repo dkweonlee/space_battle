50eed0410f26012fcb81e99f6e6937361e7b37cc
Daniel <dkweonlee@gmail.com>
2017-02-09 20:30:27 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# space_battle
+D+8 @Code Platoon.
+This is personal project which help me to understand the class on ruby.
---
+++ spaceship.rb
@@ -0,0 +1,35 @@
+class SpaceShip
+  attr_accessor :name, :health
+  def initialize(name, health)
+    @name = name 
+    @health = health
+  end
+
+  def fires_on target
+    target.takes_a_hit
+  end  
+
+  def takes_a_hit
+  @health = @health - 1
+  puts @name+" health = " + @health.to_s
+  end
+
+  def take_off count_down
+    until count_down <=0
+      puts "#{count_down}"
+      sleep 1
+      count_down -= 1
+    end
+    puts @name+" blasting off!!!"
+  end
+end
+enterprise = SpaceShip.new("enterprise", 10)
+
+# defiant = SpaceShip.new("defiant", 10)
+# enterprise.take_off 5
+# defiant.take_off 5
+# defiant.fires_on enterprise
+
+
+
+
