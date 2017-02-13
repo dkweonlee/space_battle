46e8aef5dd65d4c260660d80f5c4bd022d332a36
Daniel <dkweonlee@gmail.com>
2017-02-13 16:31:52 -0600

    updates on take off and fire and help

--- spaceship.rb
+++ spaceship.rb
@@ -1,39 +1,49 @@
+require 'pry'
 class SpaceShip
-  user_input = gets.chomp
-  attr_accessor :name, :health
-  def initialize(name, health)
-    @name = name 
+  attr_accessor :spaceship_name, :health, :enemy
+  def initialize(spaceship_name, enemy_name, health)
+    @spaceship_name = spaceship_name 
     @health = health
+    @enemy = enemy_name 
   end
 
-  def fires_on target
-    target.takes_a_hit
+  def fires_on (spaceship_name)   #change name to enemy later on after you figure out everything. 
+    takes_a_hit
   end  
 
   def takes_a_hit
   @health = @health - 1
-  puts @name+" health = " + @health.to_s
+  puts @spaceship_name + " health = " + @health.to_s
   end
 
-  def take_off count_down
+  def take_off (count_down = 10)
     until count_down <= 0
       puts "#{count_down}"
       sleep 1
       count_down -= 1
     end
-    puts @name + " blasting off!!!"
+    puts @spaceship_name + " blasting off!!!"
   end
   def user_command
-  if user_input == 'take off'
-    
+      user_input = gets.chomp
+  if user_input == 'takeoff'
+    take_off
+  elsif user_input == 'fire'
+    fires_on (@spaceship_name)
+  elsif user_input == 'help'
+    puts '----list of commands----'
+    p 'take off = takeoff ', 'fire = fire'
+  else
+    puts 'wrong command'
+    puts "type 'help' to see more commands"
   end
-
   end
 end
-enterprise = SpaceShip.new("enterprise", 10)
+enterprise = SpaceShip.new("enterprise", 'defiant', 10)
+enterprise.user_command
 
 # defiant = SpaceShip.new("defiant", 10)
-enterprise.take_off 5
+
 # defiant.take_off 5
 # defiant.fires_on enterprise
 
